430e058f75589a8f182b5f35d74e31a6d2ebf6eb
Fadi Dasus <fadi.dasus@ubsend.com>
2022-03-24 13:45:56 +0100

    log information level and attach it to each endpoint

--- src/main/java/com/itu/minitwitbackend/controller/TweetController.java
+++ src/main/java/com/itu/minitwitbackend/controller/TweetController.java
@@ -1,6 +1,7 @@
 package com.itu.minitwitbackend.controller;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.itu.minitwitbackend.controller.api.model.TweetFlagRequest;
 import com.itu.minitwitbackend.repository.entity.TweetEntity;
 import com.itu.minitwitbackend.service.TweetService;
+import com.microsoft.applicationinsights.TelemetryClient;
 
 @RestController
 @RequestMapping("/devops/tweet")
@@ -19,29 +21,40 @@ public class TweetController {
 
     private final TweetService tweetService;
 
-    public TweetController(TweetService tweetService) {
+    private final TelemetryClient telemetryClient;
+
+    @Autowired
+    public TweetController(TweetService tweetService, TelemetryClient telemetryClient) {
         this.tweetService = tweetService;
+        this.telemetryClient = telemetryClient;
     }
 
     @GetMapping("/get-user-tweets/{username}")
     public ResponseEntity<List<TweetEntity>> getUserTweets(@PathVariable String username) {
+        telemetryClient.trackEvent("get-user-tweets /is triggered");
+
         return ResponseEntity.ok(tweetService.findByUsername(username));
     }
 
     @GetMapping("/get-all-tweets/{batch-size}/{page-number}")
     public ResponseEntity<List<TweetEntity>> getAllTweets(@PathVariable(value = "batch-size", required = false) int batchSize,
                                                           @PathVariable(value = "page-number", required = false) int pageNumber) {
+        telemetryClient.trackEvent("get-all-tweets /is triggered");
         if (batchSize == 0) batchSize = 100;
         return ResponseEntity.ok(tweetService.getAllTweetsSorted(batchSize, pageNumber));
     }
 
     @PostMapping("/add-tweet")
     public ResponseEntity<TweetEntity> addTweets(@RequestBody TweetEntity tweet) {
+        telemetryClient.trackEvent("add-tweet /is triggered");
+
         return ResponseEntity.ok(tweetService.saveTweet(tweet));
     }
 
     @PostMapping("/update-Tweet-flag")
     public ResponseEntity<String> flagTweet(@RequestBody TweetFlagRequest tweetFlagRequest) {
+        telemetryClient.trackEvent("update-Tweet-flag /is triggered");
+
         tweetService.flagTweet(tweetFlagRequest);
         return ResponseEntity.ok("flag updated successfully");
     }
--- src/main/java/com/itu/minitwitbackend/controller/UserController.java
+++ src/main/java/com/itu/minitwitbackend/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.itu.minitwitbackend.controller.api.model.FollowUserRequest;
 import com.itu.minitwitbackend.repository.entity.UserEntity;
 import com.itu.minitwitbackend.service.UserService;
+import com.microsoft.applicationinsights.TelemetryClient;
 
 @RestController
 @Validated
@@ -22,43 +23,53 @@ import com.itu.minitwitbackend.service.UserService;
 public class UserController {
 
     private final UserService userService;
-
+    private final TelemetryClient telemetryClient;
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, TelemetryClient telemetryClient) {
         this.userService = userService;
+        this.telemetryClient = telemetryClient;
     }
 
     @GetMapping("/get-user/{username}")
     public ResponseEntity<UserEntity> getUserById(@PathVariable String username) {
+        telemetryClient.trackEvent("get-user-by-id /is triggered");
+
         return ResponseEntity.ok(userService.getUser(username).get());
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> createUser(@Valid @RequestBody UserEntity user) {
+        telemetryClient.trackEvent("register /is triggered");
+
         userService.createNewUser(user);
         return new ResponseEntity<>("", HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/login/{username}/{password}")
     public ResponseEntity login(@PathVariable String username, @PathVariable String password) {
+        telemetryClient.trackEvent("login /is triggered");
+
         userService.validateUserCredentials(username, password);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping("/follow")
     public ResponseEntity<String> followUser(@RequestBody FollowUserRequest followUserRequest) {
+        telemetryClient.trackEvent("follow-user /is triggered");
         userService.followUser(followUserRequest);
         return new ResponseEntity<>("done", HttpStatus.OK);
     }
 
     @PostMapping("/unfollow")
     public ResponseEntity<String> unfollowUser(@Valid @RequestBody FollowUserRequest followUserRequest) {
+        telemetryClient.trackEvent("unfollow-user /is triggered");
         userService.unfollowUser(followUserRequest);
         return new ResponseEntity<>("done", HttpStatus.OK);
     }
 
     @PostMapping("/isFollowing")
     public ResponseEntity<Boolean> isFollowing(@Valid @RequestBody FollowUserRequest followUserRequest) {
+        telemetryClient.trackEvent("is-following /is triggered");
         return new ResponseEntity<>(userService.isFollowing(followUserRequest), HttpStatus.OK);
     }
 }
